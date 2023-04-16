a717fb7311557673fd8bf0796efaeec3a007c14b
Alberto <alberto.tavares@zup.com.br>
2023-04-16 19:20:10 -0300

    comecando o projeto

---
+++ src/main/java/br/com/deveficiente/ingressos/DesafioParser1Application.java
@@ -0,0 +1,13 @@
+package br.com.deveficiente.ingressos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioParser1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioParser1Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/deveficiente/ingressos/DesafioParser1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.deveficiente.ingressos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioParser1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
